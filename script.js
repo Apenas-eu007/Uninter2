a092d0e4aba9918e356f5940b58308135a629d21
Apenas-eu007 <gitxhub00@tuta.io>
2025-04-15 16:49:47 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById("motivacaoBtn").onclick = function() {
+        document.getElementById("mensagem").innerText = "Fiz o meu melhor, espero que goste!";
+        document.getElementById("mensagem").style.display = "block"; // Exibe a mensagem
+    };
+</script>
